d2c201410951696d719277a44ba951f795649575
Moriz Büsing <moriz.buesing@gmail.com>
2024-09-11 10:26:15 +0200

    Improve design and a11y

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <title>Next.js middleware matcher tester</title>
+        <meta
+          name="description"
+          content="Tool to test your middleware matcher patterns"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
